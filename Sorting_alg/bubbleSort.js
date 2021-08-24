139c4bc82ff6a007eb4ba05dd68724f7b6cc6aa1
Arthur Ghazaryan <newartroll11@gmail.com>
2021-08-24 18:18:15 +0400

    feat:(bubble sort)

--- Sorting_alg/bubbleSort.js
+++ Sorting_alg/bubbleSort.js
@@ -1,6 +1,5 @@
 function bubbleSort(arr) {
-  let len = arr.length;
-  for (let i = len - 1; i >= 0; i--) {
+  for (let i = arr.length - 1; i >= 0; i--) {
     for (let j = 1; j <= i; j++) {
       if (arr[j - 1] > arr[j]) {
         let temp = arr[j - 1];
