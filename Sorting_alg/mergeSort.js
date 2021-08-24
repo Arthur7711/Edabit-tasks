ff86c6a4e76afeea03b5d1b80b32d154720f4e63
Arthur Ghazaryan <newartroll11@gmail.com>
2021-08-24 18:32:55 +0400

    feat:(merge_sort)

---
+++ Sorting_alg/mergeSort.js
@@ -0,0 +1,25 @@
+function mergeSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  let mid = Math.floor(arr.length / 2),
+    left = arr.slice(0, mid),
+    right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  let result = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+  /* Either left/right array will be empty or both */
+  return [...result, ...left, ...right];
+}
+
+console.log(mergeSort([5, 3, 8, 10, 4, 1]));
