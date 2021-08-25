bd3b1940f7cf62af7adf48a72e889dc9af717fae
Arthur Ghazaryan <newartroll11@gmail.com>
2021-08-25 10:06:18 +0400

    feat:(l1)

--- Sorting_alg/quicksort.js
+++ Sorting_alg/quicksort.js
@@ -1,18 +1,32 @@
-function quicksort(array) {
-  if (array.length <= 1) {
-    return array;
-  }
+// function quicksort(array) {
+//   if (array.length <= 1) {
+//     return array;
+//   }
+//   let base = array[0];
+//   let left = [];
+//   let right = [];
 
-  let base = array[0];
+//   for (let i = 1; i < array.length; i++) {
+//     array[i] < base ? left.push(array[i]) : right.push(array[i]);
+//   }
+
+//   return quicksort(left).concat(base, quicksort(right));
+// }
+
+// let items = [5, 3, 7, 6, 2, 9];
+// console.log(quicksort(items));
+
+function quickSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  let base = arr[0];
   let left = [];
   let right = [];
-
-  for (let i = 1; i < array.length; i++) {
-    array[i] < base ? left.push(array[i]) : right.push(array[i]);
+  for (let i = 1; i < arr.length; i++) {
+    arr[i] < base ? left.push(arr[i]) : right.push(arr[i]);
   }
-
-  return quicksort(left).concat(base, quicksort(right));
+  return quickSort(left).concat(base, quickSort(right));
 }
-
 let items = [5, 3, 7, 6, 2, 9];
-console.log(quicksort(items));
+console.log(quickSort(items));
