03a3874e0599336a11efb1fcd7074be428145ff2
Arthur Ghazaryan <newartroll11@gmail.com>
2021-08-25 09:41:57 +0400

    fix:(l3)

--- Sorting_alg/bubbleSort.js
+++ Sorting_alg/bubbleSort.js
@@ -18,12 +18,12 @@ function bubbleSort(arr) {
   for (let i = arr.length - 1; i >= 0; i--) {
     for (let j = 1; j <= i; j++) {
       if (arr[j - 1] > arr[j]) {
-        let temp = arr[j - 1];
-        arr[j - 1] = arr[j];
-        arr[j] = temp;
+        arr[j - 1] = arr[j - 1] + arr[j];
+        arr[j] = arr[j - 1] - arr[j];
+        arr[j - 1] = arr[j - 1] - arr[j];
       }
     }
   }
   return arr;
 }
-console.log(bubbleSort([1, 2, 3, 4, 5, 6]));
+console.log(bubbleSort([1, 2, 2, 3, 3, 3, 4, 5, 6]));
