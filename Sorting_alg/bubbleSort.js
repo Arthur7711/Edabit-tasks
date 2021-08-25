70c066985e6a3f5a48360279f76e045d3efb2bd1
Arthur Ghazaryan <newartroll11@gmail.com>
2021-08-25 09:46:29 +0400

    feat:(bubbleSort)

--- Sorting_alg/bubbleSort.js
+++ Sorting_alg/bubbleSort.js
@@ -1,19 +1,3 @@
-// function bubbleSort(arr) {
-//   for (let i = arr.length - 1; i >= 0; i--) {
-//     for (let j = 1; j <= i; j++) {
-//       if (arr[j - 1] > arr[j]) {
-//         let temp = arr[j - 1];
-//         arr[j - 1] = arr[j];
-//         arr[j] = temp;
-//       }
-//     }
-//   }
-//   return arr;
-// }
-// console.log(bubbleSort([7, 5, 2, 4, 3, 9])); //[2, 3, 4, 5, 7, 9]
-// console.log(bubbleSort([9, 7, 5, 4, 3, 1])); //[1, 3, 4, 5, 7, 9]
-// console.log(bubbleSort([1, 2, 3, 4, 5, 6])); //[1, 2, 3, 4, 5, 6]
-
 function bubbleSort(arr) {
   for (let i = arr.length - 1; i >= 0; i--) {
     for (let j = 1; j <= i; j++) {
@@ -26,4 +10,7 @@ function bubbleSort(arr) {
   }
   return arr;
 }
-console.log(bubbleSort([1, 2, 2, 3, 3, 3, 4, 5, 6]));
+
+console.log(bubbleSort([7, 5, 2, 4, 3, 9]));
+console.log(bubbleSort([9, 7, 5, 4, 3, 1]));
+console.log(bubbleSort([1, 2, 3, 4, 5, 6]));
