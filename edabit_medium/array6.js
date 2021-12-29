4b785e6ff6a5013d8b28988abfb7644e59d325ff
Arthur <newartroll11@gmail.com>
2021-12-29 10:46:03 +0400

    feat:(array6)

---
+++ edabit_medium/array6.js
@@ -0,0 +1,13 @@
+// In this challenge you will be given an array similar to the following:
+
+// [[3], 4, [2], [5], 1, 6]
+// In words, elements of the array are either an integer or an array containing a single integer. We humans can clearly see that this array can reasonably be sorted according to "the content of the elements" as:
+
+// [1, [2], [3], 4, [5], 6]
+// Create a function that, given an array similar to the above, sorts the array according to the "content of the elements".
+
+function sortIt(arr) {
+  return arr.sort((a, b) => a - b);
+}
+
+sortIt([[4], [1], [3]]);
