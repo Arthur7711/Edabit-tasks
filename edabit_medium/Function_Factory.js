7f762d0b1d68ddbb9ab79a6abde5f2ed3c4e6e02
Arthur <newartroll11@gmail.com>
2024-03-25 01:25:45 +0400

    feat: clouser sum function

---
+++ edabit_medium/Function_Factory.js
@@ -0,0 +1,25 @@
+// Create a function that takes a "base number" as an argument. This function should return another function which takes a new argument, and returns the sum of the "base number" and the new argument.
+// Please check the examples below for a clearer representation of the behavior expected.
+
+// Calling makePlusFunction(5) returns a new function that takes an input,
+// and returns the result when adding 5 to it.
+
+// plusFive(2) ➞ 7
+
+// plusFive(-8) ➞ -3
+
+// // Calling makePlusFunction(10) returns a new function that takes an input,
+// // and returns the result when adding 10 to it.
+
+// const plusTen = makePlusFunction(10)
+
+// plusTen(0) ➞ 10
+
+// plusTen(188) ➞ 198
+
+// plusFive(plusTen(0)) ➞ 15
+
+function makePlusFunction(baseNum) {
+	return (num)=>baseNum+num
+}
+const plusFive = makePlusFunction(5)
