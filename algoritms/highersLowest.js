ff8b093e287ee7a23bcbcd4ebd05f95ad0be7b91
Arthur <newartroll11@gmail.com>
2024-03-26 09:31:00 +0400

    feat: highLow

---
+++ algoritms/highersLowest.js
@@ -0,0 +1,13 @@
+// Create a function that accepts a string of space separated numbers and returns the highest and lowest number (as a string).
+// All numbers are valid Int32, no need to validate them.
+// There will always be at least one number in the input string.
+// Output string must be two numbers separated by a single space, and highest number is first.
+// highLow("1 2 3 4 5") ➞ "5 1"
+// highLow("1 2 -3 4 5") ➞ "5 -3"
+// highLow("1 9 3 4 -5") ➞ "9 -5"
+// highLow("13") ➞ "13 13"
+
+function highLow(str) {
+	const digits = str.split(' ');
+    return `${Math.max(...digits)} ${Math.min(...digits)}`
+}
