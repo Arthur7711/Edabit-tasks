45d2eae295ed2e0d97e926fc792f270d6ac6e9c7
Arthur <newartroll11@gmail.com>
2024-05-12 17:20:42 +0400

    num_of_digits

---
+++ edabit_medium/Find Number of Digits in Number.js
@@ -0,0 +1,4 @@
+// Create a function that will return an integer number corresponding to the amount of digits in the given integer num.
+function num_of_digits(num) {
+	return num.toString().length
+}
