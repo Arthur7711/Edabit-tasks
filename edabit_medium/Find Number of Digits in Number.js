0b424325aa110d691864d0649b17e8081027c9cd
Arthur <newartroll11@gmail.com>
2024-05-12 17:27:50 +0400

    fix num_of_digits for negative numbers

--- edabit_medium/Find Number of Digits in Number.js
+++ edabit_medium/Find Number of Digits in Number.js
@@ -1,4 +1,4 @@
 // Create a function that will return an integer number corresponding to the amount of digits in the given integer num.
 function num_of_digits(num) {
-	return num.toString().length
+	return Math.abs(num).toString().length
 }
