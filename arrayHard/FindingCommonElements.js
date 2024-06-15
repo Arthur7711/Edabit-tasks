1a7d9bb49ec7621e5a785652656afe783e2d488f
Arthur <newartroll11@gmail.com>
2024-06-15 18:26:58 +0400

    feat: commonElements with set

--- arrayHard/FindingCommonElements.js
+++ arrayHard/FindingCommonElements.js
@@ -3,7 +3,14 @@
 // commonElements([1, 3, 4, 6, 7, 9], [1, 2, 3, 4, 7, 10]) ➞ [1, 3, 4, 7]
 // commonElements([1, 2, 2, 2, 3, 4, 5], [1, 2, 4, 5]) ➞ [1, 2, 4, 5]
 // commonElements([1, 2, 3, 4, 5], [10, 12, 13, 15]) ➞ []
+
+// function commonElements(arr1, arr2) {
+//   return arr1.filter((el) => arr2.find((item) => item === el));
+// }
+// console.log(commonElements([-1, 3, 4, 6, 7, 9], [1, 3]));
+
 function commonElements(arr1, arr2) {
-  return arr1.filter((el) => arr2.find((item) => item === el));
+  const setArray = new Set(arr1);
+  return arr2.filter((el) => setArray.has(el));
 }
 console.log(commonElements([-1, 3, 4, 6, 7, 9], [1, 3]));
