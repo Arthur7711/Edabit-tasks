992a268cdd135d05c2fb4c00d7956cff3eadb6ed
Arthur <newartroll11@gmail.com>
2024-06-15 18:16:39 +0400

    feat: commonElements

---
+++ arrayHard/FindingCommonElements.js
@@ -0,0 +1,9 @@
+// // Create a function that takes two "sorted" arrays of numbers and returns an array of numbers which are common to both the input arrays.
+// commonElements([-1, 3, 4, 6, 7, 9], [1, 3]) ➞ [3]
+// commonElements([1, 3, 4, 6, 7, 9], [1, 2, 3, 4, 7, 10]) ➞ [1, 3, 4, 7]
+// commonElements([1, 2, 2, 2, 3, 4, 5], [1, 2, 4, 5]) ➞ [1, 2, 4, 5]
+// commonElements([1, 2, 3, 4, 5], [10, 12, 13, 15]) ➞ []
+function commonElements(arr1, arr2) {
+  return arr1.filter((el) => arr2.find((item) => item === el));
+}
+console.log(commonElements([-1, 3, 4, 6, 7, 9], [1, 3]));
