bc70ebf10162a8fa45adeb56c552aa609ae69a0d
Arthur <newartroll11@gmail.com>
2025-02-02 12:43:45 +0400

    feat: args work with es6

--- arguments/index.js
+++ arguments/index.js
@@ -3,3 +3,9 @@ function arrayFromArguments() {
 }
 
 console.log(arrayFromArguments("1", 2, 4, 5, 6, 7, 8, 89));
+
+// the same work with es6 version
+function getParamItems(...args) {
+  return args;
+}
+console.log(getParamItems("222222", 2, 4, 5, 6, 7, 8, 89));
