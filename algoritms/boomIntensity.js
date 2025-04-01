77f185832f7d6c74078b8804d543d2b2fa93dda6
Arthur <newartroll11@gmail.com>
2025-04-02 00:27:53 +0400

    feat: boomIntensity handle by loops

--- algoritms/boomIntensity.js
+++ algoritms/boomIntensity.js
@@ -9,14 +9,35 @@
 // A number which is evenly divisible by 2 and 5 will have both effects applied (see example #4).
 // "Boom" will always start with a capital "B", except when n is less than 2, then return a minature explosion as "boom".
 // Examples
-boomIntensity(4); // ➞ "Boooom!"
+console.log(boomIntensity(4)); // ➞ "Boooom!"
 // There are 4 "o"s and 4 is divisible by 2 (exclamation mark included)
-
-boomIntensity(1); //➞ "boom"
+console.log(boomIntensity(1)); //➞ "boom"
 // 1 is lower than 2, so we return "boom"
-
-boomIntensity(5); //➞ "BOOOOOM"
+console.log(boomIntensity(5)); //➞ "BOOOOOM"
 // There are 5 "o"s and 5 is divisible by 5 (all caps)
-
-boomIntensity(10); //➞ "BOOOOOOOOOOM!"
+console.log(boomIntensity(10)); //➞ "BOOOOOOOOOOM!"
 // There are 10 "o"s and 10 is divisible by 2 and 5 (all caps and exclamation mark included)
+
+function boomIntensity(num) {
+  if (num < 2) {
+    return "boom";
+  } else if (num % 2 === 0 && num % 5 !== 0) {
+    let oLetters = "";
+    for (let i = 0; i < num; i++) {
+      oLetters += "o";
+    }
+    return `B${oLetters}m!`;
+  } else if (num % 2 !== 0 && num % 5 === 0) {
+    let oLetters = "";
+    for (let i = 0; i < num; i++) {
+      oLetters += "O";
+    }
+    return `B${oLetters}M`;
+  } else if (num % 2 === 0 && num % 5 === 0) {
+    let oLetters = "";
+    for (let i = 0; i < num; i++) {
+      oLetters += "O";
+    }
+    return `B${oLetters}M!`;
+  }
+}
