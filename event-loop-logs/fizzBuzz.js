be97501bdb011e6dc69ac2e1726640f714094f86
Arthur <newartroll11@gmail.com>
2025-04-05 20:17:19 +0400

    feat: fizbuzz task

---
+++ event-loop-logs/fizzBuzz.js
@@ -0,0 +1,11 @@
+// The Fizz Buzz Test
+// Write a method that returns array of all the numbers from 1 to an integer argument. But for multiples of three use “Fizz” instead of the number and for the multiples of five use “Buzz”. For numbers which are multiples of both three and five use “FizzBuzz”.
+// Notes
+// Make sure to return an array.
+
+// Example
+console.log(fizzBuzz(10)); // ➞ [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz"]
+console.log(fizzBuzz(15)); // ➞ [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]
+function fizzBuzz(num) {
+    
+}
