be4d10cd01ef9d7242c7087b8cdb962404e6807b
Arthur <newartroll11@gmail.com>
2025-04-12 13:29:03 +0400

    feat: largestEven task desc

---
+++ algoritms/largestEven.js
@@ -0,0 +1,6 @@
+// Find the Largest Even Number
+// Write a function that finds the largest number in an array nums that is also even. If there is no even number, return -1.
+// Examples
+console.log(largestEven([3, 7, 2, 1, 7, 9, 10, 13])); // ➞ 10
+console.log(largestEven([1, 3, 5, 7])); // ➞ -1
+console.log(largestEven([0, 19, 18973623])); // ➞ 0
