a224c78873ef8460b622a550c25ff7601b8ff19e
Arthur <newartroll11@gmail.com>
2025-04-12 13:14:26 +0400

    feat: mystery func task

---
+++ algoritms/mysteryFunc.js
@@ -0,0 +1,10 @@
+// Reverse Coding Challenge #6
+// This is a reverse coding challenge. Normally you're given explicit directions with how to create a function. Here, you must generate your own function to satisfy the relationship between the inputs and outputs.
+// Your task is to create a function that, when fed the inputs below, produces the sample outputs shown.
+// Examples
+// here it'll we like all digits *
+console.log(mysteryFunc(152)); //➞ 10
+console.log(mysteryFunc(832)); //➞ 48
+console.log(mysteryFunc(19)); //➞ 9
+console.log(mysteryFunc(133)); //➞ 9
+
