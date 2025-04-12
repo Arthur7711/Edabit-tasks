9475a08ad0082b1ce026968295819736eaad1335
Arthur <newartroll11@gmail.com>
2025-04-12 13:18:09 +0400

    feat: dnaToRna task

---
+++ algoritms/dnaToRna.js
@@ -0,0 +1,9 @@
+// Transcribe to mRNA
+// Transcribe the given DNA strand into corresponding mRNA - a type of RNA, that will be formed from DNA after transcription. DNA has the bases A, T, G and C, while RNA converts to U, A, C and G respectively.
+// Notes
+// Transcription is the process of making complementary strand.
+// A, T, G and C in DNA converts to U, A, C and G respectively in mRNA.
+// Examples
+console.log(dnaToRna("ATTAGCGCGATATACGCGTAC")); //➞ "UAAUCGCGCUAUAUGCGCAUG"
+console.log(dnaToRna("CGATATA")); //➞ "GCUAUAU"
+console.log(dnaToRna("GTCATACGACGTA")); //➞ "CAGUAUGCUGCAU"
