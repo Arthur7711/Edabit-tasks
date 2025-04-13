22cad73234e580bd95386a40ed6cf4a5f1d2e02a
Arthur <newartroll11@gmail.com>
2025-04-13 18:31:18 +0400

    feat: chessBoard task desc

---
+++ algoritms/chessBoard.js
@@ -0,0 +1,5 @@
+// Create a function that takes a chess board square's coordinate and return its color.
+// Examples
+console.log(chessBoard("a1")); //➞ "black"
+console.log(chessBoard("e5")); //➞ "black"
+console.log(chessBoard("d1")); //➞ "white"
