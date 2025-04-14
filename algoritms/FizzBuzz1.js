75178553c90af5b5b01eb3653be631e2cd2700e1
Arthur <newartroll11@gmail.com>
2025-04-14 23:55:46 +0400

    feat: fb task close

--- algoritms/FizzBuzz1.js
+++ algoritms/FizzBuzz1.js
@@ -6,3 +6,20 @@
 console.log(fizzBuzz(10)); //➞ [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz"]
 
 console.log(fizzBuzz(15)); // ➞ [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]
+
+function fizzBuzz(num) {
+  const result = [];
+  for (let i = 1; i <= num; i++) {
+    const el = array[i];
+    if (el % 5 === 0 && el % 3 === 0) {
+      result.push("FizzBazz");
+    } else if (el % 5 === 0) {
+      result.push("Bazz");
+    } else if (el % 3 === 0) {
+      result.push("Fizz");
+    } else {
+      result.push(el);
+    }
+  }
+  return result;
+}
