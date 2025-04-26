1dcb516581a0396daf1e87abe0eb206b8ae92c49
Arthur <newartroll11@gmail.com>
2025-04-26 19:44:33 +0400

    feat: task doubleFactorial

---
+++ recursions/doubleFactorial.js
@@ -0,0 +1,13 @@
+// Double Factorial
+// Create a function that takes a number num and returns its double factorial.
+// Notes
+// Assume all input values are greater than or equal to -1.
+// Try to solve it with recursion.
+// Double factorial is not the same as factorial * 2.
+
+// Examples
+// 9*7*5*3*1 = 945
+console.log(doubleFactorial(0)); // ➞ 1
+console.log(doubleFactorial(2)); // ➞ 2
+console.log(doubleFactorial(9)); // ➞ 945
+console.log(doubleFactorial(14)); // ➞ 645120
