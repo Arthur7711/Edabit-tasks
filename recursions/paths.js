9f826107bed27cfe9f2dcb1b9317cdab995ba071
Arthur <newartroll11@gmail.com>
2025-04-26 19:14:36 +0400

    feat: paths task

---
+++ recursions/paths.js
@@ -0,0 +1,20 @@
+// Travelling Salesman Problem
+// A salesman has a number of cities to visit. He wants to calculate the total number of possible paths he could take, visiting each city once before returning home. Return the total number of possible paths a salesman can travel, given n cities.
+// If we have cities A, B and C, possible paths would be:
+// A -> B -> C
+// A -> C -> B
+// B -> A -> C
+// B -> C -> A
+// C -> B -> A
+// C -> A -> B
+// ... which gives us 6 as the possible number of paths.
+// Examples
+
+console.log(paths(4)); // ➞ 24
+console.log(paths(1)); // ➞ 1
+console.log(paths(9)); // ➞ 362880
+
+//
+function paths(num) {
+    
+}
