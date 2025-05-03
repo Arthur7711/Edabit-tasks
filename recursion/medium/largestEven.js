9d2548777c406f46e0dc0e1111de925ef4c11f46
Arthur <newartroll11@gmail.com>
2025-05-03 20:34:07 +0400

    feat: largestEven task resolve

--- recursion/medium/largestEven.js
+++ recursion/medium/largestEven.js
@@ -1,6 +1,14 @@
 // Find the Largest Even Number
 // Write a function that finds the largest number in an array nums that is also even. If there is no even number, return -1.
 // Examples
-largestEven([3, 7, 2, 1, 7, 9, 10, 13]); // ➞ 10
-largestEven([1, 3, 5, 7]); // ➞ -1
-largestEven([0, 19, 18973623]); // ➞ 0
+console.log(largestEven([3, 7, 2, 1, 7, 9, 10, 13])); // ➞ 10
+console.log(largestEven([1, 3, 5, 7])); // ➞ -1
+console.log(largestEven([0, 19, 18973623])); // ➞ 0
+
+function largestEven(arr, i = 0, max = -1) {
+  if (i === arr.length) return max;
+  if (arr[i] % 2 === 0 && arr[i] > max) {
+    max = arr[i];
+  }
+  return largestEven(arr, i + 1, max);
+}
