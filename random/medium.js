830b7236f87d23fd11a6c50d273e7ee5e9d41002
Arthur <newartroll11@gmail.com>
2025-11-08 03:20:57 +0400

    feat: redundant

--- random/medium.js
+++ random/medium.js
@@ -1,3 +1,5 @@
+// 1)
+
 // How Much is True?
 // Create a function which returns the number of true values there are in an array.
 // Notes
@@ -5,11 +7,30 @@
 // All array items are of the type bool (true or false).
 
 // Examples
-function countTrue(booleans) {
-  return booleans.filter(Boolean).length;
+// function countTrue(booleans) {
+//   return booleans.filter(Boolean).length;
+// }
+// console.log(countTrue([true, false, false, true, false])); // ➞ 2
+
+// console.log(countTrue([false, false, false, false])); // ➞ 0
+
+// console.log(countTrue([])); // ➞ 0
+
+// 2)
+// A Redundant Function
+// Write a function redundant that takes in a string str and returns a function that returns str.
+// Notes
+// Your function should return a function, not a string.
+
+// Examples
+function redundant(str) {
+  return () => str;
 }
-console.log(countTrue([true, false, false, true, false])); // ➞ 2
+const f1 = redundant("apple");
+console.log(f1()); //➞ "apple"
 
-console.log(countTrue([false, false, false, false])); // ➞ 0
+const f2 = redundant("pear");
+console.log(f2()); //➞ "pear"
 
-console.log(countTrue([])); // ➞ 0
+const f3 = redundant("");
+console.log(f3()); //➞ ""
